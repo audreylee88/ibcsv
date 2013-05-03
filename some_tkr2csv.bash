dfdb2e0a77d005e669dabc1758b157d35855dc58
Dan Bikle <dan@bot4.us>
2013-05-03 23:24:41 +0000

    some_tkr2csv.bash

--- some_tkr2csv.bash
+++ some_tkr2csv.bash
@@ -5,6 +5,7 @@
 # I use this script to implement some of my ideas described here:
 # /pt/s/r/ibcsv/readme.txt
 
+set -x
 cd /pt/s/r/ibcsv/
 
 # Any file older than 5 days should be removed from 
@@ -15,6 +16,16 @@ rsync -a /pt/s/r/ibcsv /bak/
 cd /bak/ibcsv/csv_files_old/
 cp -p /etc/hosts.allow random_old_file.gz
 find . -type f -name '*.gz' -mtime +5 -print | xargs ls -l
+find . -type f -name '*.gz' -mtime +5 -print | xargs rm -f
+
+# Move files older than 1 day from 
+# /bak/ibcsv/csv_files/
+# to
+# /bak/ibcsv/csv_files_old/
+# and compress them
+cd /bak/ibcsv/csv_files/
+cp -p /etc/hosts.allow random_old_file.csv
+find . -type f -name '*.csv' -mtime +1 -print | xargs ls -l
 
 exit
 
