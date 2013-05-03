57aaa17848f0f0f15c4f8cf9deef868573d533ef
Dan Bikle <dan@bot4.us>
2013-05-03 19:34:13 +0000

    1tkr_dl2csv.bash

---
+++ 1tkr_dl2csv.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# /pt/s/r/ibcsv/1tkr_dl2csv.bash
+
+# I use this script to download 1 tkr to a csv file.
+
+if [ $# -ne 1 ]
+then
+  echo You need to give a tkr.
+  echo Demo:
+  echo $0 SPY
+  exit 1
+fi
+
+TKR=$1
+. ~/.orcl
+. /pt/s/r/sp3/.jruby
+export myts=`date +%Y_%m_%d_%H_%M`
+
+set -x
+
+date
+cd /pt/s/r/ibcsv/
+
+rsync -a /pt/s/r/ibcsv /bak/
+
+cd /bak/ibcsv/ibapi/
+sleep 2
+jruby req_hdata_1D.rb $TKR
+
+exit 0
