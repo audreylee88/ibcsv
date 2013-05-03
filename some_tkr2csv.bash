e03f0f0c8784252e04cdcbbf21fccd908bb35b6e
Dan Bikle <dan@bot4.us>
2013-05-03 23:50:24 +0000

    some_tkr2csv.bash

--- some_tkr2csv.bash
+++ some_tkr2csv.bash
@@ -30,9 +30,12 @@ find . -type f -name '*.csv' -mtime +1 -print | xargs ls -l
 # see if -1 works okay:
 # find . -type f -mtime -1 -print | xargs ls -l
 
-find . -type f -name '*.csv' -mtime +1 -exec mv {} /bak/ibcsv/csv_files_old/ \;
-ls -l /bak/ibcsv/csv_files_old/*.csv
-
+# mv the old files and gzip em:
+cd /bak/ibcsv/
+find csv_files/ -type f -name '*.csv' -mtime +1 -exec mv {} /bak/ibcsv/csv_files_old/ \;
+ls -l /bak/ibcsv/csv_files_old/*
+gzip -9f /bak/ibcsv/csv_files_old/*.csv
+ls -l /bak/ibcsv/csv_files_old/*
 
 exit
 
